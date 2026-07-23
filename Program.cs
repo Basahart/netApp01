43066e27a91dd1b285fa429c1715ca2e560026ca
Alberto Balderas <alberic@live.com.mx>
2026-07-22 23:31:13 -0600

    Carga de archivos

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace MiPrimerPrograma
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hola Mundo, esto es C#");
+
+            //Variables
+
+            /*string nombre = "Tu nombre aqui";
+
+            Console.WriteLine(nombre);
+
+            for (int i = 0; i<=10; i++)
+            {
+                Console.WriteLine(i);    
+            }
+            */
+        }
+    }
+}
+
